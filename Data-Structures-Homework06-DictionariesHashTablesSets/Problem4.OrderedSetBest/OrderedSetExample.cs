274336d7a7985816f9e78353b10e050f8354b8f7
DimitarStoyanovVictory <Dimitar.Stoyanov.Victory@gmail.com>
2015-08-12 11:35:15 +0300

    da
    da

---
+++ Data-Structures-Homework06-DictionariesHashTablesSets/Problem4.OrderedSetBest/OrderedSetExample.cs
@@ -0,0 +1,41 @@
+﻿namespace OrderedSet
+{
+    using System;
+
+    public class OrderedSetExample
+    {
+        public static void Main()
+        {
+            var set = new OrderedSet<int>();
+            set.Add(17);
+            set.Add(9);
+            set.Add(12);
+            set.Add(6);
+            set.Add(25);
+            set.Add(4);
+            set.Add(8);
+            set.Add(10);
+            set.Add(14);
+            set.Add(18);
+            set.Add(22);
+            set.Add(20);
+            set.Add(24);
+            set.Add(28);
+            set.Add(26);
+            set.Add(30);
+            set.Add(2);
+            set.Add(1);
+
+            Console.WriteLine(set.Contains(12));
+            Console.WriteLine(set.Contains(60));
+
+            foreach (var child in set)
+            {
+                Console.WriteLine(child);
+            }
+
+            // set.Remove(12);
+            set.Remove(9);
+        }
+    }
+}
---
+++ Data-Structures-Homework06-DictionariesHashTablesSets/Problem4.OrderedSetBest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Problem4.OrderedSetBest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Problem4.OrderedSetBest")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f688c20a-27a6-48a4-a229-bf987d9d9a4e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
